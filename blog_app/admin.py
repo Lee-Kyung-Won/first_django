6c58495a5a2108f4ebc6a0626dd92c9cc9b666bb
Lee-Kyung-Won <kunglee2545@gmail.com>
2019-01-23 09:18:56 +0900

    8강. blog.admin.PostAdmin에 list_display 커스텀

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
+    list_display = ['id', 'title', 'content_size']
+
+    def content_size(self, post):
+        return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
+    content_size.short_description = '글자수'
+    #content_size.allow_tags = True  # deprecated
 
 # admin.site.register(Post, PostAdmin)
