f8933d9a9b44644aaab1e195e469dbb9feca4954
Lee-Kyung-Won <kunglee2545@gmail.com>
2019-01-23 09:39:32 +0900

    8강. blog.models.Post 모델에 status 필드를 추가하고, 이를 처리하는 Custom Admin Action 구현

--- blog_app/models.py
+++ blog_app/models.py
@@ -9,6 +9,13 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
+
+
     author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목',
         help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')   #길이 제한이 있는 문자열
@@ -20,5 +27,8 @@ class Post(models.Model):
             validators=[lnglat_validator],
             help_text='경도/위도 포맷으로 입력력')
 
-    # created_at = models.DataTimeField(auto_new_add=True)
-    # updated_at = models.DataTimeField(auto_now=True)
+
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+    #created_at = models.DataTimeField(auto_new_add=True)
+    #updated_at = models.DataTimeField(auto_now=True)
