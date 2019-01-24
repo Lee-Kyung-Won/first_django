1a483288c684ea01e6fd4bdc57c67b03d65c5285
Lee-Kyung-Won <kunglee2545@gmail.com>
2019-01-24 19:57:46 +0900

    11강. blog.models.Comment 모델 생성/admin등록

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,10 +1,11 @@
-#blog/urls.py
+#blog_app/urls.py
 
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^(?P<id>\d+)/$', views.post_detail),
 ]
 
 
